ff386cd50aaa3af3834b55f8179cd0bfc5ca6a1e
Jeremy Bui <buijeremy0@gmail.com>
2023-04-19 09:52:37 -0500

    Revised some of the code for the RPi Audio Visualizer.

--- audio_spectrum_analyzer.py
+++ audio_spectrum_analyzer.py
@@ -1,37 +1,12 @@
-#!/usr/bin/env python
-
-# (This is an example similar to an example from the Adafruit fork
-#  to show the similarities. Most important difference currently is, that
-#  this library wants RGB mode.)
-#
-# A more complex RGBMatrix example works with the Python Imaging Library,
-# demonstrating a few graphics primitives and image loading.
-# Note that PIL graphics do not have an immediate effect on the display --
-# image is drawn into a separate buffer, which is then copied to the matrix
-# using the SetImage() function (see examples below).
-# Requires rgbmatrix.so present in the same directory.
-
-# PIL Image module (create or load images) is explained here:
-# http://effbot.org/imagingbook/image.htm
-# PIL ImageDraw module (draw shapes to images) explained here:
-# http://effbot.org/imagingbook/imagedraw.htm
-
 from PIL import Image
 from PIL import ImageDraw
-import time
-from rgbmatrix import RGBMatrix, RGBMatrixOptions
-
 import pyaudio
 import numpy as np
-from PyQt5 import QtWidgets
-import pyqtgraph as pg
-import sys
-import time
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
 
 '''
-Splitting the input into even and odd parts simplifies the FFT algorithm because it reduces the number of required calculations. 
-After the split, each of the even and odd parts can be processed recursively using the same FFT algorithm. This reduces the 
-problem size by a factor of 2 at each recursion level, which leads to a significant reduction in the number of calculations required to compute the FFT.
+Computes the DFT (discrete Fourier transform) of the input data. The DFT is a transform that converts a time-domain signal into a frequency-domain representation
 '''
 def fft(data):
     n = len(data)
@@ -39,11 +14,18 @@ def fft(data):
     if n <= 1:
         return data
     # Otherwise, the input data is divided into two lists: even and odd, containing the even and odd-indexed elements of data respectively.
+    '''
+    Splitting the input into even and odd parts simplifies the FFT algorithm because it reduces the number of required calculations. 
+    After the split, each of the even and odd parts can be processed recursively using the same FFT algorithm. This reduces the 
+    problem size by a factor of 2 at each recursion level, which leads to a significant reduction in the number of calculations required to compute the FFT.
+    '''
     even = fft(data[0::2])
     odd = fft(data[1::2])
     '''
     The fft function then calculates the "twiddle factors" using the formula np.exp(-2j * np.pi * k / n), where k is the index of the sample 
     and n is the total length of the input array. It multiplies each odd-indexed sample by the corresponding twiddle factor and stores the results in a temporary array t.
+    Odd-indexed samples are multiplied by the twiddle factors because they represent the imaginary part of the frequency components. The even-indexed samples represent the 
+    real part of the frequency components and do not require a phase shift.
     '''
     t = [np.exp(-2j * np.pi * k / n) * odd[k] for k in range(n // 2)]
     # Combine the even and odd parts of the array
@@ -67,37 +49,20 @@ options = RGBMatrixOptions()
 options.rows = 32
 options.chain_length = 1
 options.parallel = 1
-options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
+options.hardware_mapping = 'regular' 
 
 matrix = RGBMatrix(options = options)
 
-# RGB example w/graphics prims.
-# Note, only "RGB" mode is supported currently.
-image = Image.new("RGB", (32, 32), color='black')  # Can be larger than matrix if wanted!!
- # Declare Draw instance before prims
-# Draw some shapes into image (no immediate effect on matrix)...
+#Set up Image that will be drawn
+image = Image.new("RGB", (32, 32), color='black')
 
+# Declare Draw instance
 draw = ImageDraw.Draw(image)
 
 while True:
-    
-
-    
-    '''
-    draw.rectangle((31, 31, 31, 31), fill='white')
-    matrix.SetImage(image, 0, 0)
-    
-    time.sleep(0.5)
-    draw.rectangle((31, 31, 31, 31), fill='black')
-    matrix.SetImage(image, 0, 0)
-    
-    time.sleep(0.5)
-    matrix.Clear()
-    matrix.SetImage(image, 0, 0)
-    '''
-    
+ 
     # Set up the FFT
-    fft_x = np.linspace(0, RATE/2, int(CHUNK / 2))
+    fft_x = np.linspace(0, RATE/2, int(CHUNK / 2)) # Returns an evenly spaced array of 512 nums from 0 to 22050
 
     # Set the maximum amplitude threshold in volts
     max_amplitude_threshold = 0.1
@@ -105,14 +70,21 @@ while True:
     data = stream.read(1024, exception_on_overflow = False)
     data = np.frombuffer(data, dtype=np.int16)
 
-    # Scale the data to range from -1 to 1
+    '''
+    Because we are using a 16 bit integer, the range is currently between [-32768, 32767]
+    We scale this down to [-1, 1]
+    '''
     data = data / 32768.0
 
     # Apply the limiter to the data
     data = np.clip(data, -max_amplitude_threshold, max_amplitude_threshold)
 
     # Perform the FFT
-    fft_data = np.array(fft(data)) / len(data)
+    # Scaling, so divide by len(data) to have the values be a representation with respect to the CHUNK
+    fft_data = np.array(fft(data)) / len(data) 
+    # The output of the FFT is symmetric around the midpoint of the array, so we can discard one half of the array without losing any information.
+    # We then multiply by 2 to account for the half that was discarded since the magnitudes there matter.
+    # So we only consider the data of the first half up to the Nyquist Frequency.
     fft_data = np.abs(fft_data[:int(len(fft_data) / 2)]) * 2
     
     
@@ -142,6 +114,8 @@ while True:
         '''
 
         # Find the FFT index with the highest amplitude in the current range
+        # Added by start_index to account for the index of the full len(fft_data) because we are only analyzing
+        # a subset at the moment.
         max_index = np.argmax(fft_data[start_index:end_index]) + start_index
 
         # Calculate the amplitude for the current range and multiply by a constant factor
