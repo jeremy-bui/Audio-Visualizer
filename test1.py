a8ceb5744f3e2674966b724f4aef228f648dbc66
Jeremy Bui <90273699+jeremy-bui@users.noreply.github.com>
2023-04-21 22:52:34 +0100

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+    audio_option = sys.argv[1]
+    frequency_option = sys.argv[2]
+
+    print(f"Selected audio option: {audio_option}")
+    print(f"Selected frequency option: {frequency_option}")
+
+if __name__ == "__main__":
+    main()
